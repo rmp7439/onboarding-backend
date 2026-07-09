af2950b2524e7a8e4f08a484f5b56749369a0204
Rudra Patni <coder7439@gmail.com>
2026-07-09 08:47:09 +0530

    tweaked server code

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import app from './app';
 import { env } from './config/env';
+import { AuthService } from './services/auth/auth.service';
 
-const startServer = (): void => {
+const startServer = async (): Promise<void> => {
   try {
+    await AuthService.createInitialAdmin();
+
     app.listen(env.PORT, () => {
-      console.log(`[server]: Server is running in ${env.NODE_ENV} mode at http://localhost:${env.PORT}`);
+      console.log(
+        `[server]: Server is running in ${env.NODE_ENV} mode at http://localhost:${env.PORT}`
+      );
     });
   } catch (error) {
     console.error('[server]: Failed to start the server', error);
