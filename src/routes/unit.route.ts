b06faf112d2289308328cfc9fce128ed39ae061e
Rudra Patni <coder7439@gmail.com>
2026-07-25 12:54:29 +0530

    fixed routing issue

--- src/routes/unit.route.ts
+++ src/routes/unit.route.ts
@@ -1,17 +1,41 @@
-import { Router, RequestHandler } from 'express';
-import { 
-  getUnits, 
-  createUnit, 
-  updateUnit, 
-  deleteUnit 
-} from '../controllers/unit.controller';
-import { authenticateToken, RequireRole, ROLES } from '../middleware/auth.middleware';
+import { Router, RequestHandler } from "express";
+import {
+  getUnits,
+  createUnit,
+  updateUnit,
+  deleteUnit,
+} from "../controllers/unit.controller";
+import {
+  authenticateToken,
+  RequireRole,
+  ROLES,
+} from "../middleware/auth.middleware";
 
 const router = Router();
 
-router.get('/units', authenticateToken as RequestHandler, RequireRole(ROLES.DEV, ROLES.ADMIN) as RequestHandler, getUnits as RequestHandler);
-router.post('/units', authenticateToken as RequestHandler, RequireRole(ROLES.DEV) as RequestHandler, createUnit as RequestHandler);
-router.put('/units/:id', authenticateToken as RequestHandler, RequireRole(ROLES.DEV) as RequestHandler, updateUnit as RequestHandler);
-router.delete('/units/:id', authenticateToken as RequestHandler, RequireRole(ROLES.DEV) as RequestHandler, deleteUnit as RequestHandler);
+router.get(
+  "/units",
+  authenticateToken,
+  RequireRole(ROLES.SUPERVISOR, ROLES.DEV, ROLES.ADMIN),
+  getUnits,
+);
+router.post(
+  "/units",
+  authenticateToken as RequestHandler,
+  RequireRole(ROLES.DEV) as RequestHandler,
+  createUnit as RequestHandler,
+);
+router.put(
+  "/units/:id",
+  authenticateToken as RequestHandler,
+  RequireRole(ROLES.DEV) as RequestHandler,
+  updateUnit as RequestHandler,
+);
+router.delete(
+  "/units/:id",
+  authenticateToken as RequestHandler,
+  RequireRole(ROLES.DEV) as RequestHandler,
+  deleteUnit as RequestHandler,
+);
 
-export default router
+export default router;
